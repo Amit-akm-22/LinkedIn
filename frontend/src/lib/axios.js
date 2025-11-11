41699aa617298ef3fde77917030979a49c8f21e2
Amit Manmode <24cs10am22@mitsgwl.ac.in>
2025-11-12 02:18:43 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,7 @@
+
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  // ✅ Direct backend API base URL (Render)
   baseURL: "https://linkedin-backend-k3cs.onrender.com/api/v1",
-  withCredentials: true, // Include cookies for authentication
+  withCredentials: true,
 });
-
