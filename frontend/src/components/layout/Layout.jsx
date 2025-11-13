77f721935a43bad5a623c1a43f9da69ac879601e
Amit Manmode <24cs10am22@mitsgwl.ac.in>
2025-11-13 10:48:10 +0530

    Update Layout.jsx

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -1,11 +1,15 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 
-const Layout = ({ children }) => {
+const Layout = () => {
 	return (
-		<div className='min-h-screen bg-base-100'>
+		<div className="min-h-screen bg-base-100">
 			<Navbar />
-			<main className='max-w-7xl mx-auto px-4 py-6'>{children}</main>
+			<main className="max-w-7xl mx-auto px-4 py-6">
+				<Outlet /> {/* 👈 This is where pages like HomePage, NotificationsPage, etc. will render */}
+			</main>
 		</div>
 	);
 };
+
 export default Layout;
